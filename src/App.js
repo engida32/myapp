5cc2315391d920448b50f0765390bd28f8c396d9
enguwerk <enguwerk@outlook.com>
2019-11-29 23:06:35 -0800

    this git commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./component/navbar";
-import User  from "./component/users/Users";
-import Mdb from "./mdv/ButtonPage"
-import axios from "axios"
+import User from "./component/users/Users";
+import Mdb from "./mdv/ButtonPage";
+import axios from "axios";
+import Navbar1 from "./component/Navbar1";
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  };
 
-  state ={
-    users:[],
-    loading:false
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
   }
 
-async componentDidMount(){
-  this.setState({loading:true});
-
-  const res=await axios.get("https://api.github.com/users");
-  this.setState({users:res.data,loading:false});
-  
-}
-
   render() {
-
     return (
       <div className="App">
-        <Navbar title='github finder'  icon='fab fa-github'  />
-        <div className='container'>
-        <User loading={this.state.loading} users={this.state.users}/>
+        <div>
+          <Navbar1 title="github finder" icon="fab fa-github" />
         </div>
-        <div className='card' >
-          <Mdb/>
+        <div className="container">
+          <User loading={this.state.loading} users={this.state.users} />
+        </div>
+        <div className="card">
+          <Mdb />
         </div>
       </div>
     );
